e26b17ec022cbe7fcc3600c7a2ff78c94ba1dd27
DeviceBlack <65994174+Device-Black@users.noreply.github.com>
2023-05-04 19:29:11 -0300

    update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
 
-# Verifica se um arquivo foi passado como argumento
 if [ $# -ne 1 ]; then
-  echo -e "\nUso: $0 <arquivo>\n"
-  exit 1
+  echo "Utilize: "$0" <arquivo.pwn>"
+  exit 0
 fi
 
-# Extrai o nome do arquivo
-arquivo=$1
-nome_arquivo=$(basename $arquivo .pwn)
+FILE=$1
+FILENAME=$(basename $FILE .pwn)
 
-# Extrai o nome do arquivo
-arquivo=$1
-nome_arquivo=$(basename $arquivo .pwn)
-
-# Compila o arquivo usando o comando pawncc
-pawncc '/sdcard/PawnCC/'$arquivo '-C+' '-(+' '-;+' '-O0' '-i/sdcard/PawnCC/include' '-e/sdcard/PawnCC/errors.txt'
-
-# Move o arquivo compilado para o diretório /home/user/compilados
-mv $HOME'/'$nome_arquivo'.amx' '/sdcard/PawnCC' 2>'/dev/null' && echo -e "\nSucesso ao tentar compilar o script '"$arquivo"'\n" || echo -e "\nFalha ao tentar compilar o script '"$arquivo"'\n" 
+pawncc /sdcard/PawnCC/$FILE -C+ '-;+' '-(+' -O0 -i/sdcard/PawnCC/include -e/sdcard/PawnCC/errors.txt
+mv $FILENAME.amx /sdcard/PawnCC 2>/dev/null && echo $FILE "foi compilado com sucesso" || echo "Falha ao tentar compilar" $FILE
