ed42767168fe0961b504feb280860ae787152006
DeviceBlack <65994174+Device-Black@users.noreply.github.com>
2023-05-04 18:28:57 -0300

    update install.sh

--- install.sh
+++ install.sh
@@ -4,13 +4,14 @@
 mv $HOME'/termux-pawn/PawnCC' '/sdcard/'
 
 # Definir permissoes
-chmod +x $HOME'/termux-pawn/compile.sh'
 chmod +x $HOME'/termux-pawn/libpawnc.so'
+chmod +x $HOME'/termux-pawn/compile.sh'
 chmod +x $HOME'/termux-pawn/pawncc'
 
 # Mover arquivos
+mv $HOME'/termux-pawn/libpawnc.so' '/lib/'
 mv $HOME'/termux-pawn/compile.sh' $HOME
-mv pawncc '/bin/' && mv libpawnc.so '/lib/'
+mv $HOME'/termux-pawn/pawncc' '/bin/'
 
 # Exibir comando
 echo -e "\nUso: ./compile.sh <arquivo>\n"
