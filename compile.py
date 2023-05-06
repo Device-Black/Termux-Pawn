216d237e35a7900c947d283de75bbb79d8664273
DeviceBlack <65994174+Device-Black@users.noreply.github.com>
2023-05-06 03:49:36 -0300

    update compile.py

--- compile.py
+++ compile.py
@@ -26,6 +26,7 @@ def Extrair_Arquivos():
 	try:
 		print('Extraindo "termux-pawn.zip", aguarde...')
 		zipfile.ZipFile("termux-pawn.zip", 'r').extractall("./")
+		os.remove("termux-pawn.zip")
 		print('Extracao concluida!')
 	except zipfile.BadZipFile:
 		print("Erro ao extrair arquivo ZIP: arquivo invalido.")
